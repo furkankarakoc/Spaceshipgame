001990882537496e00e6d273fd6bb91cfa35ab58
furkankarakoc <ffurkankarakoc@gmail.com>
2022-05-12 17:59:46 +0300

    fix favorite station error

--- a862805181fc4fe9e9a7d51a9b92b170/AppDelegate.swift
+++ a862805181fc4fe9e9a7d51a9b92b170/AppDelegate.swift
@@ -41,7 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "Uzay")
+        let container = NSPersistentContainer(name: "a862805181fc4fe9e9a7d51a9b92b170")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
